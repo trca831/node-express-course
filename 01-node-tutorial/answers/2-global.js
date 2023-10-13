70acda1f0dacd8cd7a9068702bc268955c454134
Tracy Cano <tracycano3.14@gmail.com>
2023-10-13 15:49:47 -0700

    Updated global and prompter

--- 01-node-tutorial/answers/2-global.js
+++ 01-node-tutorial/answers/2-global.js
@@ -6,7 +6,7 @@
 // process - info about env wher the program is being executed
 // module - info about current module (file)
 
-//export MY_VAR="Hi there!" inside terminal
+//export MY_VAR = "Hi there!" //inside terminal
 
-console.log(__dirname)
-console.log(process.env.MY_VAR)
+console.log(__dirname);
+console.log(process.env.MY_VAR);
