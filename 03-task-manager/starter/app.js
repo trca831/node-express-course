12f3dfd8e7a4ffc7dba9ebd406d0fab585a37228
Tracy Cano <tracycano3.14@gmail.com>
2023-12-22 02:31:32 -0800

    basic structure of all the routes

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,18 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+
+//middleware is express json
+//if we dont use this we wont have data in rec.body
+app.use(express.json())
+
+//routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+const port = 3000 
+
+app.listen(port, console.log(`Server is listening on port ${port}...`))
