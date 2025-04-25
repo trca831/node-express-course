7f1844af94788ee0d6c9b9986d5bded0b72c4d39
Tracy Cano <tracycano3.14@gmail.com>
2025-04-24 23:48:14 -0700

    finished adding name

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -10,13 +10,22 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
     // console.log(req.query);
-    const { featured } = req.query;
+    const { featured, company, name } = req.query;
 
     const queryObject = {}
 
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false
     }
+
+    if (company) {
+        queryObject.company = company
+    }
+
+    if (name) {
+        queryObject.name = name
+    }
+
 
     console.log(queryObject);
     const products = await Product.find(queryObject);
